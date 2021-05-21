1640f8deced07887a96570dca1a5fd4f684f66f6
hanseongv <gmdjd234@gmail.com>
2021-05-22 01:29:08 +0900

    피격 테스터 고블린 넉백과 색상 변경

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -9,11 +9,14 @@ public class Enemy : MonoBehaviour
 
     private Rigidbody rigid;
     private BoxCollider boxCollider;
+    private Material mat;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
+        // 메테리얼은 위처럼 바로 못가져옴 아래처럼 마지막에 .material을 붙여야함.
+        mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,13 +25,42 @@ public class Enemy : MonoBehaviour
         {
             Weappon wepon = other.GetComponent<Weappon>();
             curHealth -= wepon.damage;
-            Debug.Log("Melee : " + curHealth);
+            Vector3 reactVec = transform.position - other.transform.position;
+            StartCoroutine(onDamage(reactVec));
         }
         else if (other.tag == "Volt")
         {
             Volt volt = other.GetComponent<Volt>();
             curHealth -= volt.damage;
-            Debug.Log("Range : " + curHealth);
+            Vector3 reactVec = other.transform.position - transform.position;
+            reactVec.y = 0;
+            Destroy(other.gameObject);
+            StartCoroutine(onDamage(reactVec));
+        }
+    }
+
+    private IEnumerator onDamage(Vector3 reactVec)
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        if (curHealth > 0)
+        {
+            mat.color = Color.white;
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+        }
+        else
+        {
+            mat.color = Color.gray;
+            gameObject.layer = 11;
+
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+
+            rigid.AddForce(reactVec * 10, ForceMode.Impulse);
+
+            Destroy(gameObject, 4);
         }
     }
 }
--- Assets/Script/Volt.cs
+++ Assets/Script/Volt.cs
@@ -6,13 +6,25 @@ public class Volt : MonoBehaviour
 {
     public int damage;
 
-    private void OnCollisionEnter(Collision collision)
+    // 볼트에서 이즈트리거 설정을 했기 때문에 아래 콜리전엔터말고 트리거 엔터로 바꿈.
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Floor")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //    else if (collision.gameObject.tag == "Wall")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Floor")
+        if (other.gameObject.tag == "Floor")
         {
             Destroy(gameObject);
         }
-        else if (collision.gameObject.tag == "Wall")
+        else if (other.gameObject.tag == "Wall")
         {
             Destroy(gameObject);
         }
